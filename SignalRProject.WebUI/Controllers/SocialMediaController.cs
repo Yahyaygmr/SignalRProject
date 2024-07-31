25172d5d684b332e7cd28c612af38540e7396507
Yahyaygmr <yahyaygmr@gmail.com>
2024-07-31 17:28:24 +0300

    social media and testimonial dtos and controllers were created

---
+++ SignalRProject.WebUI/Controllers/SocialMediaController.cs
@@ -0,0 +1,84 @@
+﻿using DynamicConsume;
+using Microsoft.AspNetCore.Mvc;
+using SignalRProject.WebUI.Dtos.SocialMediaDtos;
+
+namespace SignalRProject.WebUI.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        private readonly Consume<ResultSocialMediaDto> _resultSocialMediaConsume;
+        private readonly Consume<CreateSocialMediaDto> _createSocialMediaConsume;
+        private readonly Consume<object> _deleteSocialMediaConsume;
+        private readonly Consume<GetSocialMediaByIdDto> _getSocialMediaByIdConsume;
+        private readonly Consume<UpdateSocialMediaDto> _updateSocialMediaConsume;
+
+        public SocialMediaController(Consume<ResultSocialMediaDto> resultSocialMediaConsume, Consume<CreateSocialMediaDto> createSocialMediaConsume, Consume<object> deleteSocialMediaConsume, Consume<GetSocialMediaByIdDto> getSocialMediaByIdConsume, Consume<UpdateSocialMediaDto> updateSocialMediaConsume)
+        {
+            _resultSocialMediaConsume = resultSocialMediaConsume;
+            _createSocialMediaConsume = createSocialMediaConsume;
+            _deleteSocialMediaConsume = deleteSocialMediaConsume;
+            _getSocialMediaByIdConsume = getSocialMediaByIdConsume;
+            _updateSocialMediaConsume = updateSocialMediaConsume;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var socialMedias = await _resultSocialMediaConsume.GetListAsync("socialMedias/socialMedialist");
+
+            return View(socialMedias);
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateSocialMedia()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateSocialMedia(CreateSocialMediaDto dto)
+        {
+            var result = await _createSocialMediaConsume.PostAsync("socialMedias/createsocialMedia", dto);
+            if (result > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Kaydetme işlemi sırasında bir hata oluştu";
+                return View(dto);
+            }
+        }
+        public async Task<IActionResult> DeleteSocialMedia(int id)
+        {
+            var result = await _deleteSocialMediaConsume.DeleteAsync("socialMedias/delete", id);
+            if (result > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Silme işlemi sırasında bir hata oluştu";
+                return RedirectToAction("Index");
+            }
+
+        }
+        [HttpGet]
+        public async Task<ActionResult> UpdateSocialMedia(int id)
+        {
+            var values = await _getSocialMediaByIdConsume.GetByIdAsync("socialMedias/getsocialMedia", id);
+            return View(values);
+        }
+        [HttpPost]
+        public async Task<ActionResult> UpdateSocialMedia(UpdateSocialMediaDto dto)
+        {
+            var result = await _updateSocialMediaConsume.PutAsync("socialMedias/updatesocialMedia", dto);
+            if (result > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Güncelleme işlemi sırasında bir hata oluştu";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ SignalRProject.WebUI/Controllers/TestimonialController.cs
@@ -0,0 +1,84 @@
+﻿using DynamicConsume;
+using Microsoft.AspNetCore.Mvc;
+using SignalRProject.WebUI.Dtos.TestimonialDtos;
+
+namespace SignalRProject.WebUI.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        private readonly Consume<ResultTestimonialDto> _resultTestimonialConsume;
+        private readonly Consume<CreateTestimonialDto> _createTestimonialConsume;
+        private readonly Consume<object> _deleteTestimonialConsume;
+        private readonly Consume<GetTestimonialByIdDto> _getTestimonialByIdConsume;
+        private readonly Consume<UpdateTestimonialDto> _updateTestimonialConsume;
+
+        public TestimonialController(Consume<ResultTestimonialDto> resultTestimonialConsume, Consume<CreateTestimonialDto> createTestimonialConsume, Consume<object> deleteTestimonialConsume, Consume<GetTestimonialByIdDto> getTestimonialByIdConsume, Consume<UpdateTestimonialDto> updateTestimonialConsume)
+        {
+            _resultTestimonialConsume = resultTestimonialConsume;
+            _createTestimonialConsume = createTestimonialConsume;
+            _deleteTestimonialConsume = deleteTestimonialConsume;
+            _getTestimonialByIdConsume = getTestimonialByIdConsume;
+            _updateTestimonialConsume = updateTestimonialConsume;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var testimonials = await _resultTestimonialConsume.GetListAsync("testimonials/testimoniallist");
+
+            return View(testimonials);
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateTestimonial(CreateTestimonialDto dto)
+        {
+            var result = await _createTestimonialConsume.PostAsync("testimonials/createtestimonial", dto);
+            if (result > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Kaydetme işlemi sırasında bir hata oluştu";
+                return View(dto);
+            }
+        }
+        public async Task<IActionResult> DeleteTestimonial(int id)
+        {
+            var result = await _deleteTestimonialConsume.DeleteAsync("testimonials/delete", id);
+            if (result > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Silme işlemi sırasında bir hata oluştu";
+                return RedirectToAction("Index");
+            }
+
+        }
+        [HttpGet]
+        public async Task<ActionResult> UpdateTestimonial(int id)
+        {
+            var values = await _getTestimonialByIdConsume.GetByIdAsync("testimonials/gettestimonial", id);
+            return View(values);
+        }
+        [HttpPost]
+        public async Task<ActionResult> UpdateTestimonial(UpdateTestimonialDto dto)
+        {
+            var result = await _updateTestimonialConsume.PutAsync("testimonials/updatetestimonial", dto);
+            if (result > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Güncelleme işlemi sırasında bir hata oluştu";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
