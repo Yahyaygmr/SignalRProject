f0e1aaf1c27afed0266e026c28ab706b52a4bb34
Yahyaygmr <yahyaygmr@gmail.com>
2024-08-05 10:43:30 +0300

    get category count

--- SignalRProject.Api/Hubs/SignalRHub.cs
+++ SignalRProject.Api/Hubs/SignalRHub.cs
@@ -1,8 +1,20 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalRProject.BusinessLayer.Abstracts.Generics;
 
 namespace SignalRProject.Api.Hubs
 {
     public class SignalRHub : Hub
     {
+        private readonly IServiceManager _serviceManager;
+
+        public SignalRHub(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+        public async Task SendCategoryCount()
+        {
+            var value = _serviceManager.categoryService.EntityTable.Count();
+            await Clients.All.SendAsync("RecieveCategoryCount", value);
+        }
     }
 }
